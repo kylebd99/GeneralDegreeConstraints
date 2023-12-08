72b2e58f98f62b29a03b490b9654c66b880df160
kylebd99 <kdeeds@cs.washington.edu>
2023-12-08 11:16:32 -0800

    add datasets

--- experiments/test-calculator.jl
+++ experiments/test-calculator.jl
@@ -1,20 +1,31 @@
 include("../src/GeneralDegreeConstraints.jl")
 
-datasets = [
-            "data/wordnet.csv",
+
+datasets = ["data/wordnet.csv",
             "data/yeast.csv",
             "data/aids.csv",
             "data/dblp.csv",
             "data/postLinks.csv",
             "data/comments.csv",
-]
+            "data/movie_companies.csv",
+            "data/movie_keyword.csv",
+            "data/movie_info.csv",
+            "data/cast_info.csv",
+            "data/census13.csv",
+            "data/forest10.csv",
+            "data/power7.csv",
+            "data/dmv11.csv",
+            ]
+
+datasets = ["data/movie_info.csv"]
 
 for dataset in datasets
     data = csv_to_vec_of_vec(dataset)
     println("Dataset: ", dataset)
-    result = get_constraint_and_partition(data, 1; method=annealing)
+    result = get_constraint_and_partition(data, 1; method=greedy)
     println("Max GDC: ", result.max_gdc)
+    println("Min DC: ", minimum(values(result.regular_dcs)))
     println("Regular DCs: ", result.regular_dcs)
     println("Partition DCs: ", result.partition_dcs)
-    println("Partition sizes: ", [length(x) for x in values(result.partitions)])
+    println("Partition sizes: ", [x=>length(p) for (x, p) in result.partitions])
 end
